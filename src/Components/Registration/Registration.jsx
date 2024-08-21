e2043c98f87b9389cfee2731f5eda0ca2ff60270
KamathN593 <nk2017@kamaths.com>
2024-08-20 23:28:56 -0400

    added images to the body

--- src/Components/Registration/Registration.jsx
+++ src/Components/Registration/Registration.jsx
@@ -6,7 +6,7 @@ const Registration = () => {
         <h1 className='aboutTitle'>Membership</h1>
         <div className='container'>
             <p className='aboutTxt'> <b>How To Join? : </b>To apply for membership, prospective members can either join our Discord server (https://discord.gg/xfDzKmFzCT) or email us at LANarchyOSU@gmail.com.</p>
-            
+            {/* <img src="" alt="" /> */}
         </div>
         
             
